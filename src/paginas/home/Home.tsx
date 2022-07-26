06e587779d04cb9a330285f4311db13426f7650e
Beatriz <beatriizramoss@gmail.com>
2022-07-26 19:59:08 -0300

    Fix: imagem

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -6,7 +6,7 @@ function Home() {
     return (
     <>
         <h1 className="titulo">Home</h1>
-        <img src="./src/paginas/home/space.jpeg" alt="Imagem da tela inicial" className="img"/>
+        <img src='/space.jpeg' alt="Imagem da tela inicial" className="img"/>
     </>
     );
 }
