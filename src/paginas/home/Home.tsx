1ae5614dbb79eff5ba4b285a12d6df6a1df7e379
Beatriz <beatriizramoss@gmail.com>
2022-07-26 19:43:04 -0300

    Feat: Criando primeiros componentes com react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.css';
+
+
+function Home() {
+    return (
+    <>
+        <h1 className="titulo">Home</h1>
+        <img src="./src/paginas/home/space.jpeg" alt="Imagem da tela inicial" className="img"/>
+    </>
+    );
+}
+
+export default Home;
